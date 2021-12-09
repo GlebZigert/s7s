77447b15735d88a77b2e3b2636695fb46357eb53
Andrey Proskurnin <andrey.s7dev@yandex.ru>
2021-12-09 16:55:57 +0300

    Minor code cleaning

--- main.go
+++ main.go
@@ -15,25 +15,16 @@ const host = "0.0.0.0:2973"
 
 func main() {
     var ctx context.Context
+    //log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
     ctx, cancel := context.WithCancel(context.Background())
-
+    
     c := make(chan os.Signal, 1) // use buffer (size = 1), so the notifier are not blocked
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-
 	
     go func() {
         <-c
         log.Println("Let's stop")
         cancel()
-        /*for nil == ctx.Err() {
-            select {
-                case <-c:
-                    log.Println("Let's stop")
-                    cancel()
-                case <-time.After(3 * time.Second):
-                    log.Println("I'm running...")
-                }
-        }*/
 	}()
     
     dispatcher.Run(ctx, host)
