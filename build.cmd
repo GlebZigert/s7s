5753d9fc411c9c89cae57439d8f06853b44604ab
Andrey Proskurnin <andrey.s7dev@yandex.ru>
2022-05-16 08:40:44 +0300

    Strip debug info in build scripts

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ FOR /F "tokens=*" %%a in ('git describe --tags --always --abbrev^=0') do SET VER
 
 set "FLAGS=-X 'main.Version=%VERSION%' -X 'main.BuildTime=%DATE% %TIME:~0,8%' -X 'main.Commit=%COMMIT%'"
 
-go build -v -ldflags="%FLAGS%"
+go build -v -ldflags="%FLAGS% -s -w"
 
 if %errorlevel% equ 0 (
     if "%~1"=="run" cls && s7server
